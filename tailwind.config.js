daebe42456b6e18209d3f83eaeed4357c97e2f49
Schousboe <jakobschousboe@hotmail.com>
2025-07-12 19:49:17 +0000

    #

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,7 @@
-
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      animation: {
-        glimmer: 'glimmer 3s ease-in-out infinite',
-      },
-      keyframes: {
-        glimmer: {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-        },
-      },
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+    plugins: []
 }
